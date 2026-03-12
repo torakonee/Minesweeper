da60cf531a3bb34659aaed8e555c81fb1fdd1d04
Gromova Milena <mlgromova@edu.hse.ru>
2026-03-13 02:35:26 +0300

    Added default parametrss.

--- Config/Config.hpp
+++ Config/Config.hpp
@@ -1,1 +1,17 @@
 #pragma once
+
+namespace ms {
+
+class Config {
+public:
+    Config() = default;
+
+    int getWidth() const { return width; }
+    int getHeight() const { return height; }
+    int getMines() const { return mines; }
+private:
+    int width = 10;
+    int height = 10;
+    int mines = 10;
+};
+}
