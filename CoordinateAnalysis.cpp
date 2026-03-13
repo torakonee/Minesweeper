a9262ad0ef59199c80d8e910e03d7f7c5a2ae064
Mary <torakonee@gmail.com>
2026-03-13 06:45:04 +0300

    Update CoordinateAnalysis.cpp

--- CoordinateAnalysis.cpp
+++ CoordinateAnalysis.cpp
@@ -6,14 +6,19 @@ namespace ms {
     if (inp.size() < 2 || !std::isalpha(inp[0])) {
       return InvalidIn {};
     }
+    bool flag = false;
+    if (inp.size() > 3 && (inp[0] == 'f' || inp[0] == 'F') && inp[1] == ' ') {
+        flag = true;
+        sws.erase(0, 2);
+    }
     int x = std::toupper(inp[0]) - 'A';
     try {
       std::string sws = inp;
       int y = std::stoi(sws.erase(0, 1)) - 1;
-      return CoordinateIn {x, y};
+      return CoordinateIn {x, y, flag};
     }
     catch (...) {
-      return IvalidIn {};
+      return InvalidIn {};
     }
   }
 }
