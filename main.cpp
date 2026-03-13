dd5ce18c6a694884f9732b80573f84c64ec1c9ef
Gromova Milena <mlgromova@edu.hse.ru>
2026-03-13 03:06:55 +0300

    Main вывод default настроек

--- main.cpp
+++ main.cpp
@@ -3,5 +3,17 @@
 #include "Config/Config.hpp"
 
 int main() {
+    ms::Config config;
 
+    ms::Board board(
+        config.getWidth(),
+        config.getHeight(),
+        config.getMines()
+    );
+
+    ms::Render render;
+
+    render.drawField(board);
+
+    return 0;
 }
