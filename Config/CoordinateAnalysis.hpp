fa4920557819c49541db59de46e703fec9f63079
Gromova Milena <mlgromova@edu.hse.ru>
2026-03-13 18:17:22 +0300

    deleted files of another people

--- Config/CoordinateAnalysis.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "InputAnalysis.hpp"
-
-namespace ms {
-class CoordinateAnalysis : public InputAnalysis {
-public:
-  Input check(const std::string& inp) const override;
-};
-}
